9f056a33a103acf3ffa162cdbbe8b54bf52e79e3
EraserNode <tonybsilvadev@gmail.com>
2022-02-23 01:54:08 -0300

    HDivider add

---
+++ src/components/Form/Divider/index.tsx
@@ -0,0 +1,15 @@
+import { Divider as ChakraDivider, Grid } from "@chakra-ui/react";
+
+
+export function HDivider(){
+  return(
+    <Grid
+      gridTemplateColumns="1fr 1fr"
+      columnGap={12}
+      opacity={0.1}
+    >
+      <ChakraDivider marginY={6} />
+      <ChakraDivider marginY={6} />
+    </Grid>
+  )
+}
